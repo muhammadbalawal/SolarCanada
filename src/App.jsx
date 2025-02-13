d814748a69e67da886ae14e805a478dc2ce4394f
balawal <balawal@DESKTOP-L42F2R8>
2025-02-13 15:51:02 -0800

    debug

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,8 @@ function App() {
     <>
      <Header />
      <Hero />
-     {/* <About /> */}
-     {/* <Events /> */}
+     <About />
+     <Events />
      {/* <Partners /> */}
      {/* <Team teamMembers={teamMembers} /> */}
      {/* <Footer /> */}
--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -11,55 +11,76 @@ export default function Events() {
   };
 
   return (
-    <div className="p-8 bg-[#d1e4da]">
-      <div className="flex flex-col items-center text-center space-y-6 py-[30px] text-xl">
-        <div className="relative w-[1066px] h-[590px]">
-          <img src={greenBox} className="absolute top-0 left-0 w-full h-full" alt="Green Box Background" />
-          <img src={icons} className="absolute top-0 left-0 w-full h-full p-6" alt="Icons" />
-          <h1 className="absolute top-0 left-[40px] w-full h-full p-6 text-3xl text-white font-bold">
+    <div className="p-4 sm:p-8 bg-[#d1e4da]">
+      <div className="flex flex-col items-center text-center space-y-4 sm:space-y-6 py-[15px] sm:py-[30px] text-base sm:text-xl">
+        {/* Green Box Container */}
+        <div className="relative w-full sm:w-[1066px] h-[250px] sm:h-[590px]">
+          <img
+            src={greenBox}
+            className="absolute top-0 left-0 w-full h-full"
+            alt="Green Box Background"
+          />
+          <img
+            src={icons}
+            className="absolute top-0 left-0 w-full h-full p-2 sm:p-6"
+            alt="Icons"
+          />
+          <h1 className="absolute top-0 left-[20px] sm:left-[40px] w-full h-full p-2 sm:p-6 text-xl sm:text-3xl text-white font-bold">
             Our Events
           </h1>
 
+          {/* Image Buttons */}
           <button
             onClick={() => openImageInNewTab(image1)}
-            className="absolute h-[450px] bottom-10 left-[500px] p-6 rotate-6 transition-transform hover:scale-105 active:scale-95"
+            className="absolute h-[200px] sm:h-[450px] bottom-4 sm:bottom-10 left-[65%] sm:left-[500px] transform -translate-x-1/2 sm:translate-x-0 p-2 sm:p-6 rotate-6 transition-transform hover:scale-105 active:scale-95"
           >
-            <img src={image1} className="h-full w-auto" alt="Newsletter 1" />
+            <img src={image1} className="h-[80%] w-auto" alt="Newsletter 1" />
           </button>
           <button
             onClick={() => openImageInNewTab(image3)}
-            className="absolute h-[450px] bottom-10 left-[250px] p-6 -rotate-6 transition-transform hover:scale-105 active:scale-95"
+            className="absolute h-[200px] sm:h-[450px] bottom-4 sm:bottom-10 left-[35%] sm:left-[250px] transform -translate-x-1/2 sm:translate-x-0 p-2 sm:p-6 -rotate-6 transition-transform hover:scale-105 active:scale-95"
           >
-            <img src={image3} className="h-full w-auto" alt="Newsletter 3" />
+            <img src={image3} className="h-[80%] w-auto" alt="Newsletter 3" />
           </button>
           <button
             onClick={() => openImageInNewTab(image2)}
-            className="absolute h-[450px] bottom-18 left-[400px] p-6 rotate-2 transition-transform hover:scale-105 active:scale-95"
+            className="absolute h-[200px] sm:h-[450px] bottom-8 sm:bottom-18 left-[53%] sm:left-[400px] transform -translate-x-1/2 sm:translate-x-0 p-2 sm:p-6 rotate-2 transition-transform hover:scale-105 active:scale-95"
           >
-            <img src={image2} className="h-full w-auto" alt="Newsletter 2" />
+            <img src={image2} className="h-[80%] w-auto" alt="Newsletter 2" />
           </button>
         </div>
 
-        <div className="w-[1066px]">
+        {/* Text Content */}
+        <div className="w-full sm:w-[1066px]">
           <p>
             This summer, we are hosting{" "}
             <span className="text-[#1b7646] font-bold">
               "Get Connected - Green Energy - Lunch & Learn Q&A"
             </span>{" "}
             events every{" "}
-            <span className="text-[#1b7646] font-bold">Friday at noon</span>, to provide a platform for participants to learn from experienced guest speakers and discuss the latest in green energy solutions.
+            <span className="text-[#1b7646] font-bold">Friday at noon</span>, to
+            provide a platform for participants to learn from experienced guest
+            speakers and discuss the latest in green energy solutions.
           </p>
           <p>
             Additionally, we will be hosting our first ever{" "}
-            <span className="text-[#1b7646] font-bold">Annual General Meeting</span> on{" "}
-            <span className="text-[#1b7646] font-bold">Friday, August 16th</span>, where we will discuss our achievements, future plans, and ways you can get involved.
+            <span className="text-[#1b7646] font-bold">
+              Annual General Meeting
+            </span>{" "}
+            on{" "}
+            <span className="text-[#1b7646] font-bold">
+              Friday, August 16th
+            </span>
+            , where we will discuss our achievements, future plans, and ways you
+            can get involved.
           </p>
           <p>
             For more information on our events, please contact us at{" "}
             <span className="text-[#1b7646] font-bold underline">
               rsvp@solairecanadasolar.com
             </span>{" "}
-            to join our mailing list and receive our weekly e-newsletters. We invite you to connect, learn, and contribute to a sustainable future.
+            to join our mailing list and receive our weekly e-newsletters. We
+            invite you to connect, learn, and contribute to a sustainable future.
           </p>
         </div>
       </div>
